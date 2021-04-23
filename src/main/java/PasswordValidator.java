551ee760c90a9346bb9ff5c7b8c121b5c27ddeca
farid <farid@haiderzada.de>
2021-04-23 16:19:16 +0200

    README filled

--- src/main/java/PasswordValidator.java
+++ src/main/java/PasswordValidator.java
@@ -7,11 +7,15 @@ public class PasswordValidator {
         System.out.print("Please enter password: ");
         String input = scan.next();
 
+        //Öffnet die Methode und speichert das zurückgelieferte Ergebnis als Boolean ab.
         boolean hasMinimumLength = checkPasswordLength(input);
         boolean hasNumbersInPassword = checkPasswordForNumbers(input);
         boolean hasUpperOrLower = checkForUpperOrLowerCase(input);
+
+        //Komplette überprüfung aller Richtlinien
         boolean haseverything = checkListOfPasswords(input);
 
+        //Ausgabe ob das Passwort alle richtlinien Entspricht oder nicht.
         if (haseverything == true) {
             System.out.println("Entered password was correct!");
         } else {
@@ -19,6 +23,7 @@ public class PasswordValidator {
         }
     }
 
+    //Die Methode überprüft, ob das Passwort länger als 6 Zeichen lang ist.
     public static boolean checkPasswordLength(String input) {
         if (input.length() < 6) {
             return false;
@@ -27,8 +32,10 @@ public class PasswordValidator {
         }
     }
 
+    //Die Methode überprüft, ob das Passwort ebenfalls Zahlen enthält.
     public static boolean checkPasswordForNumbers(String input) {
         boolean isNumeric = false;
+        //Mit der for Schleife, wird jedes Zeichen im Passwort auf eine Zahl überprüft.
         for (int i = 0; i < input.length(); i++) {
             if (Character.isDigit(input.charAt(i))) {
                 isNumeric = true;
@@ -37,19 +44,20 @@ public class PasswordValidator {
         return isNumeric;
     }
 
+    //Die Methode überprüft, ob das Passwort sowohl Klein, als auch Großbuchstaben hat.
     public static boolean checkForUpperOrLowerCase(String input) {
 
         boolean isUpper = false;
         boolean isLower = false;
+        //Mit der for Schleife, wird jedes Zeichen im Passwort auf eine Groß und Kleinbuchstaben überprüft.
         for (int i = 0; i < input.length(); i++) {
             if (Character.isUpperCase(input.charAt(i))) {
                 isUpper = true;
             } else if (Character.isLowerCase(input.charAt(i))) {
-
                 isLower = true;
             }
         }
-
+        //Nur wenn beides vorhanden ist (Klein und Großbuchstabe) wird ein True zurückgegeben.
         if (isUpper == true && isLower == true) {
             return true;
         } else {
@@ -58,19 +66,28 @@ public class PasswordValidator {
 
     }
 
+    //Überprüfung alle Richtlinien
     public static boolean checkListOfPasswords(String input) {
+        //Überprüfung erster Richtlinie (Länge des Passworts)
         boolean valPass = checkPasswordLength(input);
+        //Nur wenn die erste Richtlinie korrekt ist geht es hier weiter. Sonst Zeile 88
         if (valPass == true) {
+            //Überprüfung der zweiten Richtlinie (Zahlen im Passwort)
             valPass = checkPasswordForNumbers(input);
+            //Nur wenn die zweite Richtlinie ebenfalls korrekt ist, geht es weiter. Sonst Zeile 84
             if (valPass == true) {
+                //Letzte Überprüfung der Richtlinie (Groß und Kleinschreibung).
+                // Sollte das Ergebnis korrekt sein, wird in der Variablen valPass True gespeichert.
                 valPass = checkForUpperOrLowerCase(input);
             } else {
+                //Rückgabe das die zweite Richtlinie falsch ist.
                 return valPass;
             }
         } else {
+            //Rückgabe das die erste Richtilinie falsch ist.
             return valPass;
         }
-
+        //Allgemeine Rückgabe.
         return valPass;
     }
 
